d46a7f2c1f5589aef516ca9f8877e369964d1a1a
CharSV5 <charlenebastians@hotmail.com>
2018-06-16 11:31:33 +0100

    green tests checking is divisible by 3

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -17,8 +17,16 @@ var _ = Describe("isDivisibleBy", func() {
     It("returns true if divisible by 15", func() {
       Expect(isDivisibleBy(30, 15)).Should(BeTrue())
     })
-    It("returns false if not divisible", func() {
+    It("returns false if not divisible by 15", func() {
       Expect(isDivisibleBy(29, 15)).Should(BeFalse())
+    })
+  })
+  Context("When i is divided by 3", func() {
+    It("returns true if divisible by 3", func() {
+      Expect(isDivisibleBy(6, 3)).Should(BeTrue())
+    })
+    It("returns false if not divisible by 3", func() {
+      Expect(isDivisibleBy(7, 3)).Should(BeFalse())
     })
   })
 })
