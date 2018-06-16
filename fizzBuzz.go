289f177c6c53b038dd962bc3281f358c17ab3bb7
CharSV5 <charlenebastians@hotmail.com>
2018-06-16 12:00:04 +0100

    refactoring

--- fizzBuzz.go
+++ fizzBuzz.go
@@ -1,8 +1,8 @@
 package main
 
-// import (
-//   "fmt"
-// )
+func main() {
+  isDivisibleBy(30, 15)
+}
 
 func isDivisibleBy(i int, num int) bool {
   return  i % num == 0
--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -15,26 +15,34 @@ func TestCart(t *testing.T) {
 var _ = Describe("isDivisibleBy", func() {
   Context("When i is divided by 15", func() {
     It("returns true if divisible by 15", func() {
-      Expect(isDivisibleBy(30, 15)).Should(BeTrue())
+      Expect(isDivisibleBy(30, 15)).To(BeTrue())
     })
     It("returns false if not divisible by 15", func() {
-      Expect(isDivisibleBy(29, 15)).Should(BeFalse())
+      Expect(isDivisibleBy(29, 15)).To(BeFalse())
     })
   })
   Context("When i is divided by 3", func() {
     It("returns true if divisible by 3", func() {
-      Expect(isDivisibleBy(6, 3)).Should(BeTrue())
+      Expect(isDivisibleBy(6, 3)).To(BeTrue())
     })
     It("returns false if not divisible by 3", func() {
-      Expect(isDivisibleBy(7, 3)).Should(BeFalse())
+      Expect(isDivisibleBy(7, 3)).To(BeFalse())
     })
   })
   Context("When i is divided by 5", func() {
     It("returns true if divisible by 5", func() {
-      Expect(isDivisibleBy(10, 5)).Should(BeTrue())
+      Expect(isDivisibleBy(10, 5)).To(BeTrue())
     })
     It("returns false if not divisible by 5", func() {
-      Expect(isDivisibleBy(11, 5)).Should(BeFalse())
+      Expect(isDivisibleBy(11, 5)).To(BeFalse())
     })
   })
 })
+
+// Describe("main", func() {
+//   Context("When ran", func() {
+//     It("replaces numbers divisible by 15, 3 and 5 with FizzBuzz, Fizz and Buzz", func() {
+//       Expect(main())Should
+//     })
+//   })
+// })
