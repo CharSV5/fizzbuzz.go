c07269b94870bb83be2f10b8074f23d7a662c9fc
CharSV5 <charlenebastians@hotmail.com>
2018-06-16 11:28:25 +0100

    testing that if number not divisible by 15, it returns false

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -13,9 +13,12 @@ func TestCart(t *testing.T) {
 }
 
 var _ = Describe("isDivisibleBy", func() {
-  Context("When i is divisible by 15", func() {
-    It("returns true", func() {
+  Context("When i is divided by 15", func() {
+    It("returns true if divisible by 15", func() {
       Expect(isDivisibleBy(30, 15)).Should(BeTrue())
+    })
+    It("returns false if not divisible", func() {
+      Expect(isDivisibleBy(29, 15)).Should(BeFalse())
     })
   })
 })
